829e9ba5adf44d8fa069899dfb408ec7710b4100
Eli0uce <93868767+Eli0uce@users.noreply.github.com>
2022-09-26 14:32:36 +1100

    first commit

---
+++ js/music-player.js
@@ -0,0 +1,124 @@
+    // Variables
+    var img = document.getElementById('img');
+    var title = document.getElementById('title');
+    var author = document.getElementById('author');
+    var album = document.getElementById('album')
+    var player = document.getElementById('player');
+    var volume = document.getElementById('volume');
+    var position = document.getElementById('position');
+    var currentTime= document.getElementById('currentTime');
+    var durationTime = document.getElementById('durationTime');
+
+    document.getElementById('title').innerHTML = title;
+
+    // Init
+    player.src = playlist[0].source;
+    player.volume = volume.value / 100;
+    position.value = player.currentTime;
+    img.src = playlist[0].img;
+    title.innerText = playlist[0].title;
+    author.innerText = playlist[0].author;
+    album.innerText = playlist[0].album;
+
+    // Timer
+    setInterval(function () {
+        position.value = player.currentTime * (100 / player.duration);
+
+        var currentMinutes = Math.floor(player.currentTime / 60);
+        var currentSeconds = Math.floor(player.currentTime - currentMinutes * 60);
+        var durationMinutes = Math.floor(player.duration / 60);
+        var durationSeconds = Math.floor(player.duration - durationMinutes * 60);
+        // Changement de musique automatique
+        if (currentSeconds == durationSeconds && currentMinutes == durationMinutes) {
+            onNext();
+        }
+        if (currentSeconds < 10) currentSeconds = "0" + currentSeconds;
+        if (durationSeconds < 10) durationSeconds = "0" + durationSeconds;
+        if (currentMinutes < 10) currentMinutes = "0" + currentMinutes;
+        if (durationMinutes < 10) durationMinutes = "0" + durationMinutes;
+
+        currentTime.innerText = currentMinutes + ":" + currentSeconds;
+        durationTime.innerText = durationMinutes + ":" + durationSeconds;
+    }, 1000);
+
+    var playButton = document.getElementById("play-button");
+        
+    function onLecture() {
+        
+        if(playButton.src == "http://127.0.0.1:5502/img/play.png"){
+            player.play();
+            playButton.src = "http://127.0.0.1:5502/img/pause.png";
+        } else if (playButton.src == "http://127.0.0.1:5502/img/pause.png") {
+            player.pause();
+            playButton.src = "http://127.0.0.1:5502/img/play.png";
+        }
+    }
+
+    var nolikeButton = document.getElementById("no-like");
+
+    function onLike() {
+        
+        if(nolikeButton.src == "http://127.0.0.1:5502/img/no-like.png"){
+            //player.play();
+            nolikeButton.src = "http://127.0.0.1:5502/img/like.png";
+        } else if (nolikeButton.src == "http://127.0.0.1:5502/img/like.png") {
+            //player.pause();
+            nolikeButton.src = "http://127.0.0.1:5502/img/no-like.png";
+        }
+    }
+
+    function onPrev() {
+        var currentIndex = playlist.findIndex(function(item) {                                   
+            if(player.src.search(item.source) != -1) return true;
+            else return false;
+        });
+
+        if (currentIndex == 0) {
+            var song = playlist[playlist.length-1];
+        } else {
+            var song = playlist[--currentIndex];
+        }
+
+        player.src = song.source;
+        img.src = song.img;
+        title.innerText = song.title;
+        author.innerText = song.author;
+        album.innerText = song.album;
+
+        player.play();
+        playButton.src = "http://127.0.0.1:5502/img/pause.png";
+    }
+
+    function onNext() {
+        var currentIndex = playlist.findIndex(function(item) {
+            if(player.src.search(item.source) != -1) return true;
+            else return false;
+        });
+        if (currentIndex == playlist.length-1) {
+            var song = playlist[0];
+        } else {
+            var song = playlist[++currentIndex];
+        }
+
+        player.src = song.source;
+        img.src = song.img;
+        title.innerText = song.title;
+        author.innerText = song.author;
+        album.innerText = song.album;
+
+        player.play();
+        playButton.src = "http://127.0.0.1:5502/img/pause.png";
+    }
+
+    function onPosition(value) {
+        var currentTime = player.duration * (value / 100);
+        player.currentTime = currentTime;
+    }
+    
+    function onVolume(value) {
+        player.volume = value / 100;
+    }
+
+    if (currentTime == durationTime) {
+        
+    }
---
+++ js/playlist.js
@@ -0,0 +1,54 @@
+var playlist = [
+    {
+        "img": "./img/LaytoGhostTown.jpg",
+        "title": "Ghost Town",
+        "author": "Layto",
+        "album": "Ghost Town",
+        "source": "./music/GhostTown.mp3",
+    },
+    {
+        "img": "./img/ArcandoHabits.jpg",
+        "title": "Habits",
+        "author": "Arcando",
+        "album": "Stay High",
+        "source": "./music/Habits.mp3",
+    },
+    {
+        "img": "./img/NbhdGoMode.jpg",
+        "title": "Go Mode",
+        "author": "Nbhd Nick",
+        "album": "Go Mode",
+        "source": "./music/GoMode.mp3",
+    },
+    {
+        "img": "./img/NoLie.jpg",
+        "title": "No Lie",
+        "author": "Sean Paul ft. Dua Lipa",
+        "album": "Mad Love The Prequel",
+        "source": "./music/NoLie.mp3",
+    },
+    /*{
+        "img": "./img/NoLie.jpg",
+        "title": "No Lie",
+        "author": "Sean Paul ft. Dua Lipa",
+        "source": "./music/NoLie.mp3",
+    },
+    {
+        "img": "./img/NoLie.jpg",
+        "title": "No Lie",
+        "author": "Sean Paul ft. Dua Lipa",
+        "source": "./music/NoLie.mp3",
+    },
+    {
+        "img": "./img/NoLie.jpg",
+        "title": "No Lie",
+        "author": "Sean Paul ft. Dua Lipa",
+        "source": "./music/NoLie.mp3",
+    },
+    {
+        "img": "./img/NoLie.jpg",
+        "title": "No Lie",
+        "author": "Sean Paul ft. Dua Lipa",
+        "source": "./music/NoLie.mp3",
+    },*/
+];
